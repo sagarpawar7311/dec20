c83a402fa4a55462d61fe34c9f5d0c15ff18eb2a
shubham <shubham@ShubhamP>
2020-12-07 20:38:17 +0530

    [shubham] odd even

---
+++ src/oddeveb/Evenodd.java
@@ -0,0 +1,36 @@
+package oddeveb;
+
+import java.util.Scanner;
+
+public class Evenodd 
+{
+	public static void main(String[] args) 
+	
+	{
+		
+		
+		
+		Scanner a = new Scanner(System.in);
+		
+		System.out.println("enter any nubmer");
+		
+		int num = a.nextInt();
+		
+		if (num%2==0)
+		{
+			System.out.println("enter numer is even numb00");
+			
+		}
+		
+		else
+		{
+			System.out.println("enter numb is odd num");
+			
+		}
+		
+		
+		
+		
+	}
+
+}
