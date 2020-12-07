4918848a726587fd2ac10bc467c237e14add2fe1
my <my@my-PC>
2020-12-07 08:24:20 +0530

    [SAGAR]

---
+++ src/section_1/openfirefoxbrowser.java
@@ -0,0 +1,15 @@
+package section_1;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class openfirefoxbrowser {
+	public static void main(String[] args) {
+		
+	System.setProperty("webdriver.gecko.driver", "D:\\softwar java\\firefox26\\geckodriver.exe");
+	WebDriver driver=new FirefoxDriver();
+	 driver.get("https://www.google.com/");
+	}
+
+}
